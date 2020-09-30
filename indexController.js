749f2c8d1a3bb4b362883f6494cee5c6671eead6
jimmycarney <jimmycarneytufts@gmail.com>
2020-09-30 18:51:56 -0400

    Created separate controller file

---
+++ indexController.js
@@ -0,0 +1,16 @@
+/*myApp refers to an HTML element in which the application will run*/
+var app = angular.module('myApp', []);
+/*create controller - controls the application data, pass in $scope as argument*/
+/*myCtrl is a javascript function*/
+app.controller('myCtrl', function($scope, $http) {
+    /*scope is an object with the available properties and methods*/
+    $scope.name="Jimmy";
+    $scope.names=[{name:'Jimmy',work:'ISC'},{name:'Randy',work:'ISC'},{name:'Zach',work:'VUMC'}];
+});
+/*create directive - must use camel case when defining and use dashes
+with all lowercase in the html*/
+app.directive("testDirective", function() {
+    return {
+        template : "<p>Test directive constructor<p>"
+    };
+});
