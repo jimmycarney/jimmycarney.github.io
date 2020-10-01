341e2adf975c1e0e099751a1b083fc10044952a6
jimmycarney <jimmycarneytufts@gmail.com>
2020-09-30 22:26:57 -0400

    Added moveis to movie div

--- indexController.js
+++ indexController.js
@@ -10,23 +10,36 @@ app.controller('myCtrl', function($scope, $http) {
     $scope.home = true;
     $scope.movies = false;
     $scope.football = false;
+
+    //loadContent takes in a tab name and displays that tab content
+    //while hiding all other content
     $scope.loadContent = function(tabName) {
         if (tabName == 'home') {
+            document.getElementById('homeTab').style.background = "#495469";
+            document.getElementById('moviesTab').style.background = "";
+            document.getElementById('footballTab').style.background = "";
             $scope.home = true;
             $scope.movies = false;
             $scope.football = false;
         }
         else if (tabName == 'movies') {
+            document.getElementById('homeTab').style.background = "";
+            document.getElementById('moviesTab').style.background = "#495469";
+            document.getElementById('footballTab').style.background = "";
             $scope.home = false;
             $scope.movies = true;
             $scope.football = false;
         }
         else{
+            document.getElementById('homeTab').style.background = "";
+            document.getElementById('moviesTab').style.background = "";
+            document.getElementById('footballTab').style.background = "#495469";
             $scope.home = false;
             $scope.movies = false;
             $scope.football = true;
         }
     }
+
 
 });
 /*create directive - must use camel case when defining and use dashes
